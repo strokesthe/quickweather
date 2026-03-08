cf8a896e32ab43a19a0e3661a3022933f726404b
Frederik Born <macbook@MacBook.fritz.box>
2026-03-08 22:52:21 +0100

    Add city input field with state management

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,21 @@ import { useState } from 'react'
 import './App.css'
 
 function App() {
+  const [cityName, setCityName] = useState("")
+
+  const handleCityNameInput = (e) => {
+    setCityName(e.target.value)
+  }
+
+  return (
+    <div>
+      <input
+        type='text'
+        placeholder='Stadtname'
+        onChange={handleCityNameInput}
+      />
+    </div>
+  )
 }
 
 export default App
